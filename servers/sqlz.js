468906c8ce03a446c2141590d9352c16ae72a360
Bhanu-code <bhanuchowhan652@gmail.com>
2024-05-26 13:22:46 +0530

    sqlz.css fix

--- servers/sqlz.js
+++ servers/sqlz.js
@@ -8,7 +8,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // SUPPORT FOR JSON & PUBLIC FOLDER
-app.use("/static", express.static(__dirname + "/../public"));
+app.use(express.static(__dirname + "/../public"));
+
 app.use(express.json());
 
 // SETTING VIEW ENGINE AS EJS
